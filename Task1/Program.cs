a1e6de8696af192dbf04ead45dd5a03f59860858
YaroslavPolinkin <ipz222_pyas@student.ztu.edu.ua>
2022-11-12 12:18:24 +0200

    chenged 1-3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("1");
+Console.WriteLine("2");
+Console.WriteLine("3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
